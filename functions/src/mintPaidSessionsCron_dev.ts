779f99bbf62f836f3d2de52f122bdae124ed485c
happygaijin <jesse@guavajellyco.com>
2021-11-04 10:54:40 -0700

    PR review changes

--- functions/src/mintPaidSessionsCron_dev.ts
+++ functions/src/mintPaidSessionsCron_dev.ts
@@ -1,11 +1,11 @@
 import * as functions from "firebase-functions";
 import { runScheduledFunction } from "./lib/runScheduledFunction";
 
-const API_PATH = '/sendAuthCodes';
+const API_PATH = '/mintPaidSessions';
 
 /**
- * CRON function that runs every 5 minutes
+ * CRON function that runs every 1 minute
  */
-export const sendAuthCodesCronDev = functions.pubsub
-  .schedule("every 5 minutes")
+export const mintPaidSessionsCron_dev = functions.pubsub
+  .schedule('every 1 minutes')
   .onRun(async () => await runScheduledFunction({ path: API_PATH, env: 'dev' }));
--- functions/src/sendAuthCodesCron_dev.ts
+++ functions/src/sendAuthCodesCron_dev.ts
@@ -1,11 +1,11 @@
 import * as functions from "firebase-functions";
 import { runScheduledFunction } from "./lib/runScheduledFunction";
 
-const API_PATH = '/mintPaidSessions';
+const API_PATH = '/sendAuthCodes';
 
 /**
- * CRON function that runs every 1 minute
+ * CRON function that runs every 5 minutes
  */
-export const mintPaidSessionsCronDev = functions.pubsub
-  .schedule('every 1 minutes')
+export const sendAuthCodesCron_dev = functions.pubsub
+  .schedule("every 5 minutes")
   .onRun(async () => await runScheduledFunction({ path: API_PATH, env: 'dev' }));
