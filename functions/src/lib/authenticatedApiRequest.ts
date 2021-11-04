a155768054a479e2f2e31f62aaaae548b40b9ab8
happygaijin <jesse@guavajellyco.com>
2021-11-04 13:37:49 -0700

    * Added separate environments * Added version to package.json

--- functions/src/lib/authenticatedApiRequest.ts
+++ functions/src/lib/authenticatedApiRequest.ts
@@ -2,18 +2,13 @@ import axios from "axios";
 import * as functions from "firebase-functions";
 
 export const authenticatedApiRequest = async ({ path, env }: { path: string, env: string }) => {
-    const config = functions.config();
-    let { config: { username, pass, mainnet_url, username_dev, pass_dev, testnet_url } } = config;
-    let hosturl = mainnet_url
-    if (env != 'prod') {
-        hosturl = testnet_url;
-        username = username_dev;
-        pass = pass_dev
-    }
+    const { config } = functions.config();
+    const { host, username, pass } = config[env]
+
     const token = Buffer.from(`${username}:${pass}`).toString("base64");
 
     const response = await axios.post(
-        `${hosturl}${path}`,
+        `${host}${path}`,
         {},
         { headers: { Authorization: `Basic ${token}` } }
     );
--- functions/src/lib/runScheduledFunction.ts
+++ functions/src/lib/runScheduledFunction.ts
@@ -1,6 +1,6 @@
 import { authenticatedApiRequest } from "./authenticatedApiRequest";
 
-export const runScheduledFunction = async ({ path, env }: { path: string, env: string }): Promise<null> => {
+export const runScheduledFunction = async ({ path, env }: { path: string, env: 'prod' | 'dev' }): Promise<null> => {
     try {
         const result = await authenticatedApiRequest({ path, env });
         console.log(`successful result: ${JSON.stringify(result)}`);
