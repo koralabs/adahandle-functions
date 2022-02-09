f530c7c101d02029a4146b8838be43bf01d00fad
happygaijin <jlanderson@gmail.com>
2022-02-08 21:41:53 -0800

    adding backups

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,11 +5,11 @@ admin.initializeApp();
 /**
  * Production Crons
  */
-export { mintPaidSessionsCron } from "./mintPaidSessionsCron";
-export { mintConfirmCron } from "./mintConfirmCron";
-export { sendAuthCodesCron } from "./sendAuthCodesCron";
-export { updateActiveSessionsCron } from "./updateActiveSessionsCron";
-export { saveStateCron } from "./saveStateCron";
+// export { mintPaidSessionsCron } from "./mintPaidSessionsCron";
+// export { mintConfirmCron } from "./mintConfirmCron";
+// export { sendAuthCodesCron } from "./sendAuthCodesCron";
+// export { updateActiveSessionsCron } from "./updateActiveSessionsCron";
+// export { saveStateCron } from "./saveStateCron";
 
 /**
  * Development Crons
@@ -19,4 +19,5 @@ export { mintConfirmCron_dev } from "./mintConfirmCron_dev";
 export { sendAuthCodesCron_dev } from "./sendAuthCodesCron_dev";
 export { updateActiveSessionsCron_dev } from "./updateActiveSessionsCron_dev";
 export { saveStateCron_dev } from "./saveStateCron_dev";
+export { backupFirestore_dev } from "./backupFirestore_dev";
 
--- functions/src/lib/authenticatedApiRequest.ts
+++ functions/src/lib/authenticatedApiRequest.ts
@@ -1,17 +1,11 @@
 import axios from "axios";
 import * as functions from "firebase-functions";
 
-export const authenticatedApiRequest = async <T>({ path, env, headers }: { path: string, env: string, headers: {} }): Promise<T> => {
+export const authenticatedApiRequest = async <T>({ path, env, headers }: { path: string, env: string, headers?: {} }): Promise<T> => {
     const { config } = functions.config();
     const { host, username, pass } = config[env]
 
     const token = Buffer.from(`${username}:${pass}`).toString("base64");
-    const url = `${host}${path}`;
-    const headers = {
-        headers: {
-            Authorization: `Basic ${token}`
-        }
-    };
     const response = await axios.post(
         `${host}${path}`,
         {},
