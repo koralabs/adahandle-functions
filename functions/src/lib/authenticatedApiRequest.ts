96a67a4c4ed203110cdd7030042e2a2db8be652f
guavajellyaaron <aaron@guavajellyco.com>
2022-02-08 21:27:47 -0800

    add availableMintingServers to mintPaidSessionCron

--- functions/src/lib/authenticatedApiRequest.ts
+++ functions/src/lib/authenticatedApiRequest.ts
@@ -1,24 +1,21 @@
 import axios from "axios";
 import * as functions from "firebase-functions";
 
-export const authenticatedApiRequest = async <T>({ path, env, headers }: { path: string, env: string, headers: {} }): Promise<T> => {
+export const authenticatedApiRequest = async <T>({ path, env, headers }: { path: string, env: string, headers?: Record<string, unknown> }): Promise<T> => {
     const { config } = functions.config();
     const { host, username, pass } = config[env]
 
     const token = Buffer.from(`${username}:${pass}`).toString("base64");
     const url = `${host}${path}`;
-    const headers = {
-        headers: {
-            Authorization: `Basic ${token}`
-        }
-    };
     const response = await axios.post(
-        `${host}${path}`,
+        url,
         {},
-        { headers: { 
-            ...headers,
-            Authorization: `Basic ${token}` 
-        }}
+        {
+            headers: {
+                ...headers,
+                Authorization: `Basic ${token}`
+            }
+        }
     );
     return response.data as T;
 };
