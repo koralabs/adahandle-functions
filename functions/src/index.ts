c1663418d87424c8598b9d015a0dcdc8cf3599d1
guavajellyaaron <aaron@guavajellyco.com>
2022-05-03 10:35:14 -0700

    enable production cron

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,7 +10,7 @@ export { mintConfirmCron } from "./mintConfirmCron";
 export { sendAuthCodesCron } from "./sendAuthCodesCron";
 export { updateActiveSessionsCron } from "./updateActiveSessionsCron";
 export { saveStateCron } from "./saveStateCron";
-// export { refundCron } from "./refundCron";
+export { refundCron } from "./refundCron";
 export { backupFirestore } from "./backupFirestore";
 
 /**
@@ -21,5 +21,5 @@ export { mintConfirmCron_dev } from "./mintConfirmCron_dev";
 export { sendAuthCodesCron_dev } from "./sendAuthCodesCron_dev";
 export { updateActiveSessionsCron_dev } from "./updateActiveSessionsCron_dev";
 export { saveStateCron_dev } from "./saveStateCron_dev";
+// @NOTE - leaving this off because we don't need these to run in dev
 //export { refundCron_dev } from "./refundCron_dev";
-
